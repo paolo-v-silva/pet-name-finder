6a381b4abde6bef13489f05a206c3f54493029c5
paolosilva <silva.paolo.10@gmail.com>
2026-04-14 12:34:40 +0800

    feat: gender filter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import dogForList from './assets/dog-for-list.png'
 import { CategoryFilter, GenderFilter, PetNameList } from './components'
 
 function App() {
-  const [activeGender, setActiveGender] = useState<string | null>(null)
+  const [activeGender, setActiveGender] = useState<string>('Male')
 
   return (
     <>
@@ -14,7 +14,7 @@ function App() {
         <h2>All Pets names</h2>
         <div className="grid">
           <img src={dogForList} alt="dog" width={400} />
-          <PetNameList />
+          <PetNameList activeGender={activeGender} />
         </div>
       </section>
     </>
--- src/components/PetNameList/PetNameList.tsx
+++ src/components/PetNameList/PetNameList.tsx
@@ -2,14 +2,26 @@ import './PetNameList.css'
 import names from '../../data/names.json'
 import { useMemo, useState } from 'react'
 
-export function PetNameList() {
+interface Props {
+  activeGender: string | null
+}
+
+export function PetNameList({ activeGender }: Props) {
   const pageSize = 7
   const [page] = useState(1)
 
   const paginatedPets = useMemo(() => {
+    const filteredPets = names.data.filter((pet) => {
+      if (!activeGender || activeGender === 'Both') return true
+      if (activeGender === 'Male') return pet.gender.includes('M')
+      if (activeGender === 'Female') return pet.gender.includes('F')
+
+      return true
+    })
+
     const start = (page - 1) * pageSize
-    return names.data.slice(start, start + pageSize)
-  }, [page])
+    return filteredPets.slice(start, start + pageSize)
+  }, [activeGender, page])
 
   const middleIndex = Math.floor(paginatedPets.length / 2)
 
